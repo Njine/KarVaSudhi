572ec36cb21f84b89a68767448e181896a5466ba
njine <brikab@gmail.com>
2024-09-06 18:06:15 +0300

    module definition for postgress

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KV1Token } from './models/kv1-token.entity';
+import { SudhiNFT } from './models/sudhi-nft.entity';
+import { Ballot } from './models/ballot.entity';
+import { User } from './models/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [KV1Token, SudhiNFT, Ballot, User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([KV1Token, SudhiNFT, Ballot, User]),
+  ],
 })
 export class AppModule {}
